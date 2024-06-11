52626bec25bdb4ab513ddb29e505f8bad8e7e6c4
Ahmed Elzupeir <79317722+AhmedElzupeir@users.noreply.github.com>
2024-06-11 22:07:42 +0200

    Delete Warehouse_restrict directory

--- Warehouse_restrict/models/warehouse_users.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models, api, _
-
-
-class WarehouseRestrict(models.Model):
-	_inherit = 'res.users'
-
-	warehose_ids = fields.Many2many('stock.warehouse', string='Warehouses' )
