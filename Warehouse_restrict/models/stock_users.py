b825f29afde3d627bfff51e803d88f93e4481b3e
AhmedElzupeir <ahmed.kr99773@gmail.com>
2024-06-14 00:25:13 +0200

    __

--- Warehouse_restrict/models/stock_users.py
+++ Warehouse_restrict/models/stock_users.py
@@ -6,4 +6,4 @@ from odoo import fields, models, api, _
 class WarehouseRestrict(models.Model):
 	_inherit = 'res.users'
 
-	warehose_ids = fields.Many2many('stock.warehouse', string='Warehouses' )
+	warehouse_ids = fields.Many2many('stock.warehouse', string='Warehouses' )
